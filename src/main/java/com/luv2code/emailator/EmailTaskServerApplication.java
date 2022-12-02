2e70d018e0bcbbe5f3029a9f4198fc32b6a90d10
lzugaj <lzugaj@croz.net>
2022-12-02 15:11:02 +0100

    Initial commit

---
+++ src/main/java/com/luv2code/emailator/EmailTaskServerApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.emailator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailTaskServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailTaskServerApplication.class, args);
+    }
+
+}
